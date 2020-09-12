ff7e9ad255218fa9d8754065eca2e475f1d27073
VeraciousEquivocation <Aheide@emich.edu>
2020-09-12 18:27:21 -0400

    Updates

---
+++ precache-manifest.563e57a635b539688c20cc6f248abea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1365aaef15931c153b6ee666102e7ac",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "40648ed46a70f519c762",
+    "url": "/Portfolio/static/css/2.33636e16.chunk.css"
+  },
+  {
+    "revision": "6fdf7e132d61566b873f",
+    "url": "/Portfolio/static/css/main.9adc38e2.chunk.css"
+  },
+  {
+    "revision": "40648ed46a70f519c762",
+    "url": "/Portfolio/static/js/2.ab80389c.chunk.js"
+  },
+  {
+    "revision": "9275f22eb10b5cda8304b68993ffe7e8",
+    "url": "/Portfolio/static/js/2.ab80389c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdf7e132d61566b873f",
+    "url": "/Portfolio/static/js/main.354a66fd.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
